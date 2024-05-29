6d80746adacce5ec382d32eb64d96bfe3a18ab25
Anas Durra <me9e98.2012@gmail.com>
2024-05-29 21:17:40 +0300

    update api url

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,2 @@
-// export const URL = 'https://eop-api.onrender.com/api';
-export const URL = 'http://localhost:3000/api';
+export const URL = 'https://eop-api.onrender.com/api';
+//export const URL = 'http://localhost:3000/api';
