53b816af32c9916de18ea89b3672037607c131e2
Anas Durra <me9e98.2012@gmail.com>
2024-08-18 21:24:31 +0300

    some fixes

--- src/api/constants.js
+++ src/api/constants.js
@@ -2,4 +2,4 @@
 // export const URL = 'https://9381-89-39-107-172.ngrok-free.app/api';
 // export const URL = 'http://192.168.43.147:3000/api';
 // export const URL = 'https://6f80-169-150-196-102.ngrok-free.app/api';
-export const URL = 'http://localhost:3000/api';
+export const URL = 'http://localhost:9000/api';
